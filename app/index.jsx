f6490b55774c43f72404d7acda682211545c2a61
Burhan Jawed <71589999+tibsxi@users.noreply.github.com>
2025-08-30 22:57:47 -0400

    Added dynamic routing for todo page

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Octicons from '@expo/vector-icons/Octicons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useContext, useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
@@ -12,6 +13,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { data } from '@/data/todos';
 
 export default function Index() {
+  const router = useRouter();
+
   const [todoInput, setTodoInput] = useState('');
   const [todoData, setTodoData] = useState([]);
 
@@ -73,6 +76,10 @@ export default function Index() {
     setTodoInput('');
   };
 
+  const handlePress = (id) => {
+    router.push(`/todos/${id}`);
+  };
+
   const toggleTodo = (id) => {
     setTodoData(
       todoData.map((todo) =>
@@ -87,12 +94,16 @@ export default function Index() {
 
   const renderItem = ({ item }) => (
     <View style={styles.todoItem}>
-      <Text
-        style={[styles.todoItemText, item.completed && styles.completedText]}
-        onPress={() => toggleTodo(item.id)}
+      <Pressable
+        onPress={() => handlePress(item.id)}
+        onLongPress={() => toggleTodo(item.id)}
       >
-        {item.title}
-      </Text>
+        <Text
+          style={[styles.todoItemText, item.completed && styles.completedText]}
+        >
+          {item.title}
+        </Text>
+      </Pressable>
       <Pressable onPress={() => deleteTodo(item.id)}>
         <MaterialCommunityIcons
           name='delete-circle'
@@ -109,14 +120,12 @@ export default function Index() {
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
+          maxLength={30}
           placeholder='Add a new todo'
           placeholderTextColor='gray'
           onChangeText={setTodoInput}
           value={todoInput}
         />
-        {/* <TouchableOpacity style={styles.todoButton} onPress={addNewTodo}>
-          <Text style={styles.todoButtonText}>Add</Text>
-        </TouchableOpacity> */}
         <Pressable style={styles.inputButton} onPress={addNewTodo}>
           <Text style={styles.inputButtonText}>Add</Text>
         </Pressable>
